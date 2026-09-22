658dfd0f183ba8bac441ce9fa18de2d8050a23e9
Kali Harmen-Hue <kaliharmen@gmail.com>
2026-09-22 05:07:33 +0100

    Rename Work With Me route and use full-width hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://daretobreakfree.co.uk",
   base: "/dtbf-site",
-  output: "static"
+  output: "static",
+  redirects: {
+    "/share-your-story/": "/dtbf-site/work-with-me/"
+  }
 });
